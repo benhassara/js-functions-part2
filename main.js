b2b223d50243d15b1a785da1cb905be4bdcfc6db
benhassara <bhassara@gmail.com>
2015-07-17 10:55:06 -0600

    finished exercise

--- main.js
+++ main.js
@@ -12,11 +12,36 @@ function oldestPerson(people) {
 }
 
 function longestWord(str) {
-  var warr = str.split(" ");
+  var warr = str.split(/\W/);
   var longest = "";
   for (var i = 0; i < warr.length; i++) {
+
     if (warr[i].length > longest.length)
       longest = warr[i];
   }
   return longest;
+}
+
+function factorial(num) {
+  var total = 1;
+  for (var i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
+}
+
+function palindrome(str) {
+  var rev = "";
+  var comp = str.replace(/\s+/g, "");
+  // console.log("comp: ", comp);
+  for (var i = str.length - 1; i >= 0; i--) {
+    if (str[i] === " ")
+      continue;
+    rev += str[i];
+  }
+  // console.log(rev);
+  if (comp.toLowerCase() === rev.toLowerCase())
+    return true;
+  else
+    return false;
 }
